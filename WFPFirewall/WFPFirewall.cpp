a622bb638443baccccefc72c4071cb360e5021c2
Wojciech Zmuda <zmuda.w@gmail.com>
2023-02-21 22:37:59 +0100

    WFPFirewall: use config file
    Add an example config file that targets one IP address with 10s block.
    Parse that file and use such config entry instead of having hardcoded
    values.

--- WFPFirewall/WFPFirewall.cpp
+++ WFPFirewall/WFPFirewall.cpp
@@ -2,18 +2,16 @@
 #include <fwpmu.h>
 #include <string>
 #include <iostream>
+#include "ConfigParser.h"
 
 #pragma comment(lib, "fwpuclnt.lib")
 #pragma comment(lib, "ws2_32.lib")
 
-// The IP address to block
-const std::string BLOCKED_IP_ADDRESS ="216.58.215.78";
-
-// The duration of the block in milliseconds
-const DWORD BLOCK_DURATION_MS = 30000;
-
 int main()
 {
+    std::ifstream config("wfpfirewall.cfg");
+    ConfigParser config_parser(config);
+
     FWPM_SESSION session = { 0 };
     FWPM_FILTER filter = { 0 };
     FWPM_FILTER_CONDITION condition = { 0 };
@@ -32,12 +30,11 @@ int main()
     condition.fieldKey = FWPM_CONDITION_IP_REMOTE_ADDRESS;
     condition.matchType = FWP_MATCH_EQUAL;
     condition.conditionValue.type = FWP_UINT32;
-    condition.conditionValue.uint32 = htonl(inet_addr(BLOCKED_IP_ADDRESS.c_str()));
+    condition.conditionValue.uint32 = config_parser.entries[0].ip;
 
     // Create the filter object
     filter.layerKey = FWPM_LAYER_OUTBOUND_TRANSPORT_V4;
     filter.action.type = FWP_ACTION_BLOCK;
-    filter.filterCondition = &condition;
     filter.subLayerKey = FWPM_SUBLAYER_UNIVERSAL;
     filter.weight.type = FWP_EMPTY;
     filter.numFilterConditions = 1;
@@ -53,7 +50,7 @@ int main()
     }
 
     // Wait for the specified duration
-    Sleep(BLOCK_DURATION_MS);
+    Sleep(config_parser.entries[0].value * 1000);
 
     // Remove the filter from the WFP engine
     errorCode = FwpmFilterDeleteById0(engineHandle, filterId);
