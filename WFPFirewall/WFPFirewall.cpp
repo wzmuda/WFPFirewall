ca6c485e2ef489f24cc41f5cf8c067dc61d4eca8
Wojciech Zmuda <zmuda.w@gmail.com>
2023-02-22 21:01:16 +0100

    WFPFirewall: print welcome banner and rules to be added

--- WFPFirewall/WFPFirewall.cpp
+++ WFPFirewall/WFPFirewall.cpp
@@ -7,10 +7,37 @@
 #pragma comment(lib, "fwpuclnt.lib")
 #pragma comment(lib, "ws2_32.lib")
 
+// TODO this should be taken from argv and stored in the parser
+const char* config_file_name = "wfpfirewall.cfg";
+
+void print_banner(void) {
+    std::cout <<
+        "==============================================================================" << std::endl <<
+        "========================                     =================================" << std::endl <<
+        "========================     WFPFirewall     =================================" << std::endl <<
+        "========================                     =================================" << std::endl <<
+        "=========    Manage your network in cumbersome and inefficient way!   ========" << std::endl <<
+        "=========            (C) 20203 Wojciech Zmuda                         ========" << std::endl <<
+        "==============================================================================" <<
+        std::endl << std::endl << std::endl;
+}
+
 int main()
 {
-    std::ifstream config("wfpfirewall.cfg");
+    print_banner();
+
+    std::ifstream config(config_file_name);
     ConfigParser config_parser(config);
+    if (config_parser.entries.size() == 0) {
+        std::cerr << config_file_name << ": found no valid rules." << std::endl;
+        return 1;
+    }
+    std::cout << config_file_name << ": found " << config_parser.entries.size() << " rule" <<
+        (config_parser.entries.size() > 1 ? "s:" : ":") << std::endl;
+    for (auto& e : config_parser.entries) {
+        std::cout << "\t allow " << e.host << " for " << e.value <<
+            (e.unit == LimitType::Bytes ? " bytes" : " seconds") << std::endl;
+    }
 
     FWPM_SESSION session = { 0 };
     FWPM_FILTER filter = { 0 };
