a794ac0b2ec151e70b0286c788d33571c3c18042
Wojciech Zmuda <zmuda.w@gmail.com>
2023-02-23 01:44:51 +0100

    WFPFirewall: use FirewallEngine
    Remove naive implementation of WFP operations in the main function
    of the program. Use a dedicated module instead.
    
    Fix the program a little, so it indeed allows for the connection
    for a given period of time, THEN blocks it.

--- WFPFirewall/WFPFirewall.cpp
+++ WFPFirewall/WFPFirewall.cpp
@@ -1,11 +1,9 @@
-#include <windows.h>
-#include <fwpmu.h>
 #include <string>
 #include <iostream>
 #include "ConfigParser.h"
+#include "FirewallEngine.h"
 
-#pragma comment(lib, "fwpuclnt.lib")
-#pragma comment(lib, "ws2_32.lib")
+
 
 // TODO this should be taken from argv and stored in the parser
 const char* config_file_name = "wfpfirewall.cfg";
@@ -39,55 +37,22 @@ int main()
             (e.unit == LimitType::Bytes ? " bytes" : " seconds") << std::endl;
     }
 
-    FWPM_SESSION session = { 0 };
-    FWPM_FILTER filter = { 0 };
-    FWPM_FILTER_CONDITION condition = { 0 };
-    UINT64 filterId = 0;
-    HANDLE engineHandle = NULL;
-    DWORD errorCode = ERROR_SUCCESS;
 
-    // Create a session with the WFP engine
-    errorCode = FwpmEngineOpen(NULL, RPC_C_AUTHN_WINNT, NULL, &session, &engineHandle);
-    if (errorCode != ERROR_SUCCESS) {
-        std::cout << "Failed to open WFP engine. Error code: " << errorCode << std::endl;
-        return errorCode;
-    }
+    FirewallEngine fw;
 
-    // Set the filter conditions to match the blocked IP address
-    condition.fieldKey = FWPM_CONDITION_IP_REMOTE_ADDRESS;
-    condition.matchType = FWP_MATCH_EQUAL;
-    condition.conditionValue.type = FWP_UINT32;
-    condition.conditionValue.uint32 = config_parser.entries[0].ip;
-
-    // Create the filter object
-    filter.layerKey = FWPM_LAYER_OUTBOUND_TRANSPORT_V4;
-    filter.action.type = FWP_ACTION_BLOCK;
-    filter.subLayerKey = FWPM_SUBLAYER_UNIVERSAL;
-    filter.weight.type = FWP_EMPTY;
-    filter.numFilterConditions = 1;
-    filter.filterCondition = &condition;
-    filter.displayData.name = const_cast<wchar_t *>(L"Receive/Accept Layer Block");
-    filter.displayData.description = const_cast<wchar_t*>(L"Filter to block all outbound connections.");
-
-    // Add the filter to the WFP engine
-    errorCode = FwpmFilterAdd(engineHandle, &filter, NULL, &filterId);
-    if (errorCode != ERROR_SUCCESS) {
-        std::cout << "Failed to add filter. Error code: 0x" << std::hex << errorCode << std::endl;
-        return errorCode;
-    }
+    uint64_t fid = fw.addFilter(config_parser.entries[0].ip, config_parser.entries[0].mask, false);
 
     // Wait for the specified duration
     Sleep(config_parser.entries[0].value * 1000);
 
     // Remove the filter from the WFP engine
-    errorCode = FwpmFilterDeleteById(engineHandle, filterId);
-    if (errorCode != ERROR_SUCCESS) {
-        std::cout << "Failed to delete filter. Error code: 0x" << std::hex << errorCode << std::endl;
-        return errorCode;
-    }
+    fw.deleteFilter(fid);
 
-    // Close the session with the WFP engine
-    FwpmEngineClose(engineHandle);
+    std::cout << "BLOCKING!" << std::endl;
+    fid = fw.addFilter(config_parser.entries[0].ip, config_parser.entries[0].mask, true);
+
+
+    Sleep(config_parser.entries[0].value * 1000);
 
     return 0;
 }
